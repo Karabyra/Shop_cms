1c0a3b528eada05ea580abc385fd521b76f14882
Cawko <zaragosa666@gmail.com>
2022-01-14 09:14:39 +0200

    MVC and application structure

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('VG_ACCESS') or die('access denied');
+
+const SITE_URL = "http//cms";
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'cms';
